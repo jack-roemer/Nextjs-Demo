a3a949da378d5d312e6e56424a45c1a2d0eb5c15
Jack Roemer <jroemer1993@sdsu.edu>
2025-11-15 17:00:06 -0800

    finished setup and homepage

---
+++ nextjs_demo/components/ExploreBtn.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Image from "next/image";
+
+
+const ExploreBtn = () => {
+
+    return (
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log("explore button")}>
+            <a href="#events">
+                Explore Events
+                <Image
+                    src="/icons/arrow-down.svg"
+                    alt="down arrow icon"
+                    width={24}
+                    height={24}
+                />
+            </a>
+        </button>
+    )
+}
+
+export default ExploreBtn;
